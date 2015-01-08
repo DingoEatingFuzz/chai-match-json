75e3b191feb72937534ddb5ea21e5b610dfc4b36
Michael Lange <dingoeatingfuzz@gmail.com>
2015-01-08 11:38:27 -0800

    Writing tests, fixing bugs

--- tests/match-json.js
+++ tests/match-json.js
@@ -1,6 +1,49 @@
+var matchJSON = require('../lib/match-json');
+
+chai.use(matchJSON);
+expect = chai.expect;
+
+var FIXTURE = [
+  {
+    name: 'Object 1',
+    value: 'Value 1',
+    space: 'One'
+  },
+  {
+    name: 'Object 2',
+    value: 'Value 2',
+    space: 'One'
+  }
+];
+
 describe('match-json', function() {
-  it('should match json paths for a string');
-  it('should match json paths for an object');
-  it('should throw when json paths are not matched');
-  it('should throw when the json paths are met multiple times but the once flag is set');
+  it('should match json paths for a string', function() {
+    expect(FIXTURE.map(function(o) { return JSON.stringify(o); })).to.matchJSON({
+      '$.name'  : 'Object 2',
+      '$.value' : 'Value 2'
+    });
+  });
+
+  it('should match json paths for an object', function() {
+    expect(FIXTURE).to.matchJSON({
+      '$.name'  : 'Object 2',
+      '$.value' : 'Value 2'
+    });
+  });
+
+  it('should throw when json paths are not matched', function() {
+    expect(function() {
+      expect(FIXTURE).to.matchJSON({
+        '$.alien' : 'planets'
+      });
+    }).to.throw();
+  });
+
+  it('should throw when the json paths are met multiple times but the once flag is set', function() {
+    expect(function() {
+      expect(FIXTURE).to.matchJSON({
+        '$.space' : 'One'
+      }).once;
+    }).to.throw();
+  });
 });
