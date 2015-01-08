85a3b7927d564be04369cab71238a5b1381f7ffe
Michael Lange <dingoeatingfuzz@gmail.com>
2015-01-07 20:05:26 -0800

    Start testing

---
+++ tests/match-json.js
@@ -0,0 +1,6 @@
+describe('match-json', function() {
+  it('should match json paths for a string');
+  it('should match json paths for an object');
+  it('should throw when json paths are not matched');
+  it('should throw when the json paths are met multiple times but the once flag is set');
+});
